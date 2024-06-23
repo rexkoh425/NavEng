5ffa4c8797f3e737948538b6e27e1301cdd53141
ravi-viswa105 <raviviswa105@gmail.com>
2024-06-23 12:03:12 +0530

    Added updated Z coordinate pictures

---
+++ Image_Processing/Finished_pics/changingZ.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+folder_path = "./With_arrows"
+os.chdir('C:/Users/raviv/OneDrive/Desktop/ChangingZCoordinate/Finished_pics/Finished_pics')
+current_directory = os.getcwd()
+
+
+for file_name in os.listdir(folder_path):
+
+    img_file_path = os.path.join(folder_path, file_name)
+
+    if os.path.isfile(img_file_path):
+        base_name, extension = os.path.splitext(file_name)
+        parts = base_name.split("_")
+        node_id = parts[0]
+        parts[3] = str(int(parts[3]) * 4)
+        z_coord = parts[3]
+        print(z_coord)
+        new_filename = "_".join(parts) + extension
+        new_file_path = os.path.join("./Processed_With_arrows", new_filename)
+
+        try:
+            # Move the file to the new path
+            shutil.move(img_file_path, new_file_path)
+            print(f"File '{file_name}' successfully saved as '{new_filename}'")
+        except Exception as e:
+            print(f"Error: Unable to save file '{file_name}' - {e}")
